5a7884cf65b94a0151aa8fbb2155c28adbdda94b
bastianheinzelmann <bastian.heinzelmann@hotmail.de>
2016-04-23 11:38:52 +0200

    ScreenManager and Stuff

---
+++ Teetris/ControllerDetectScreen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+
+namespace Teetris
+{
+    class ControllerDetectScreen : Screen
+    {
+        public ControllerDetectScreen(ContentManager contentManager, EventHandler theScreenEvent) : base(theScreenEvent)
+        {
+            //hier werden dann die Assets reingeladen
+        }
+    }
+}
--- Teetris/Screen.cs
+++ Teetris/Screen.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
 
 namespace Teetris
 {
     class Screen
     {
+        protected EventHandler ScreenEvent;
+
+        public Screen (EventHandler theScreenEvent)
+        {
+            ScreenEvent = theScreenEvent;
+        }
+
+        public virtual void Update(GameTime theTime)
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+        public virtual void UnloadContent()
+        {
+
+        }
+
+        public virtual void LoadContent()
+        {
+
+        }
     }
 }
